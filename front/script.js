9f233580787d3be0a0bddffe8cc2056307053e6f
Your Name <you@example.com>
2024-05-27 16:16:04 +0500

    overall chats & chat detail template

---
+++ front/script.js
@@ -0,0 +1,14 @@
+
+let root = document.documentElement;
+
+document.getElementById("switcher").addEventListener("click", function() {
+    if (root.getAttribute('data-theme') === "dark") {
+        root.setAttribute("data-theme", "light")
+    } else {
+        root.setAttribute("data-theme", "dark")
+    };
+});
+
+(function() {
+    root.setAttribute("data-theme", "light");
+})();
