c998c4b15cfded115efc53be07fd0eed05678962
Your Name <you@example.com>
2024-06-03 02:48:22 +0500

    chats complete

--- core/static/js/chat/script.js
+++ core/static/js/chat/script.js
@@ -9,5 +9,22 @@ document.getElementById("switcher").addEventListener("click", function() {
 });
 
 (function() {
-    root.setAttribute("data-theme", "light");
+    root.setAttribute("data-theme", "dark");
 })();
+
+function dropdownFunction() {
+    document.getElementById('profileDropdown').classList.toggle('show');
+};
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            };
+        };
+    };
+};
