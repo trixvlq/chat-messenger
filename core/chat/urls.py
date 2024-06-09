da655893ad5b3a95f6703bd9d0a924609889e61b
Your Name <you@example.com>
2024-06-10 00:37:27 +0500

    complete front

--- core/chat/urls.py
+++ core/chat/urls.py
@@ -4,9 +4,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('<int:chat_id>/', chat, name='chat'),
-<<<<<<< HEAD
-=======
-    path('dima/',dima),
->>>>>>> 91b8fb9c46e6de286d649d7eed847616e0c33392
     path('test/',test),
 ]
--- core/users/views.py
+++ core/users/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def sign_up(request):
     return render(request, 'registration/sign_up.html')
+
+def sign_in(request):
+    return render(request, 'registration/sign_in.html')
